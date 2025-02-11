e5d6d57b7eca4de811ee45d2d890f0af809b74fa
gulnuma khan <gkhan02@nyit.edu>
2025-02-11 12:18:48 -0500

    added stateful dice roller widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/centered.dart';
 
-
-
+ 
 
 class GradientContainer extends StatelessWidget {
-   GradientContainer(this.startAlignment, this.endAlignment,{super.key});
+
+  GradientContainer(this.startAlignment, this.endAlignment, {super.key});
+
   Alignment startAlignment;
+
   Alignment endAlignment;
 
-  
-  
   @override
+
   Widget build(BuildContext context) {
-    return Container(
-          decoration:  BoxDecoration(
-            gradient: LinearGradient(
-              begin: startAlignment,
-                end: endAlignment,
-              colors: const [
-                Color.fromARGB(255, 26, 2, 80),
-                const Color.fromARGB(255, 9, 1, 24),
-                ],
-            ),
-          ),
-          child: Centered("Welcome")
-        );
-  }
+    var activeDiceImage = 'assets/images/dice-2.png';
+    void rollDice() {
+      activeDiceImage = 'assts/images/dice-4.png';
+    }
 
 
-}
+return Container(
+
+      decoration: BoxDecoration(
+
+        gradient: LinearGradient(
+
+          begin: startAlignment,
+
+          end: endAlignment,
+
+          colors: const [
+
+            Color.fromARGB(255, 26, 2, 80),
+
+            Color.fromARGB(255, 9, 1, 24),
+
+          ],
+
+        ),
+
+      ),
+
+      child: const Center(
+
+        child: DiceRoller(),
+
+      ),
+
+    );
